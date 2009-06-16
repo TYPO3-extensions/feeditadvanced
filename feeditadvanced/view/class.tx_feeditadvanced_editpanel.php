1c6cbefc2d3a88cb3f787399bcf2258febd6765c
benni <benni@743128fe-103e-dd11-99c4-001b210b3e58>
2009-06-16 22:00:51 +0000

    Added Ext JS javascript and stylesheet files, although we might consider using Ext Core and only D&D parts of Ext JS, as the stylesheets are not needed and also add some reset.css
    git-svn-id: https://svn.typo3.org/TYPO3v4/CoreProjects/feedit/trunk@826 743128fe-103e-dd11-99c4-001b210b3e58

--- feeditadvanced/view/class.tx_feeditadvanced_editpanel.php
+++ feeditadvanced/view/class.tx_feeditadvanced_editpanel.php
@@ -616,6 +616,18 @@ class tx_feeditadvanced_editpanel {
 		$GLOBALS['TSFE']->additionalHeaderData['scriptaculous.js'] = '<script type="text/javascript" src="typo3/contrib/scriptaculous/scriptaculous.js"></script>';
 		$GLOBALS['TSFE']->additionalHeaderData['common.js'] = '<script type="text/javascript" src="typo3/js/common.js"></script>';
 
+			// loading ext JS, rudimentary for now (should be skinnable etc and only include the JS that is needed)
+		$GLOBALS['TSFE']->additionalHeaderData['ext-base.js'] = '<script type="text/javascript" src="typo3/contrib/extjs/adapter/ext/ext-base.js"></script>';
+		$GLOBALS['TSFE']->additionalHeaderData['ext-all.js'] = '<script type="text/javascript" src="typo3/contrib/extjs/ext-all-debug.js"></script>';
+		$GLOBALS['TSFE']->additionalHeaderData['extjs.css'] = '<script type="text/javascript">
+			/*<![CDATA[*/
+				Ext.BLANK_IMAGE_URL = "typo3/gfx/clear.gif";
+			/*]]>*/
+			</script>
+			<link rel="stylesheet" type="text/css" href="typo3/contrib/extjs/resources/css/ext-all.css" />
+			<link rel="stylesheet" type="text/css" href="typo3/contrib/extjs/resources/css/xtheme-gray.css" />
+';
+
 			// load AJAX handling functions
 		$GLOBALS['TSFE']->additionalHeaderData['feEdit.js'] = '<script type="text/javascript" src="' . t3lib_extMgm::siteRelPath('feeditadvanced') . 'res/js/feEdit.js"></script>';
 		$GLOBALS['TSFE']->additionalHeaderData['fe_edit_advanced.css'] = '<link href="' .  t3lib_extMgm::siteRelPath('feeditadvanced') . 'res/css/fe_edit_advanced.css" rel="stylesheet" type="text/css" />';
