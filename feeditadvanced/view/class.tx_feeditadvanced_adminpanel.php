cfc7fbfa86a7c8d2403a05f5fac29c32a1ef4a9f
chrissitopher <chrissitopher@743128fe-103e-dd11-99c4-001b210b3e58>
2010-11-23 14:26:30 +0000

    Fixed bug #11018: Logout Button redirect does not work when TYPO3 is installed in a subdirectory.
    git-svn-id: https://svn.typo3.org/TYPO3v4/CoreProjects/feedit/trunk@3027 743128fe-103e-dd11-99c4-001b210b3e58

--- feeditadvanced/view/class.tx_feeditadvanced_adminpanel.php
+++ feeditadvanced/view/class.tx_feeditadvanced_adminpanel.php
@@ -296,7 +296,7 @@ class tx_feeditadvanced_adminpanel {
 					$this->menuBar->addItem('ContextActions', $GLOBALS['LANG']->sL('LLL:EXT:feeditadvanced/locallang.xml:previewTitle'), '', '', $GLOBALS['LANG']->sL('LLL:EXT:feeditadvanced/locallang.xml:previewTooltip'), '', 'button disabled');
 				}
 				if (in_array('logout', $tsContext)) {
-					$this->menuBar->addItem('ContextActions', $GLOBALS['LANG']->sL('LLL:EXT:lang/locallang_common.xml:logout'), $action='', $image='', $GLOBALS['LANG']->sL('LLL:EXT:feeditadvanced/locallang.xml:logoutTooltip'), $onClick='', $btnClass='', $labelClass='', '/typo3/logout.php?redirect=' . t3lib_div::getIndpEnv('TYPO3_REQUEST_URL'));
+					$this->menuBar->addItem('ContextActions', $GLOBALS['LANG']->sL('LLL:EXT:lang/locallang_common.xml:logout'), $action='', $image='', $GLOBALS['LANG']->sL('LLL:EXT:feeditadvanced/locallang.xml:logoutTooltip'), $onClick='', $btnClass='', $labelClass='', t3lib_div::getIndpEnv('TYPO3_SITE_URL') . 'typo3/logout.php?redirect=' . t3lib_div::getIndpEnv('TYPO3_REQUEST_URL'));
 				}
 				if (in_array('close', $tsContext)) {
 					$this->menuBar->addItem('ContextActions', $GLOBALS['LANG']->sL('LLL:EXT:feeditadvanced/locallang.xml:statusDeactivateEditing'), '', $image='', $GLOBALS['LANG']->sL('LLL:EXT:feeditadvanced/locallang.xml:closeTooltip'), ' onclick="' . htmlspecialchars('document.TSFE_ADMIN_PANEL_Form.elements[\'TSFE_ADMIN_PANEL[menuOpen]\'].value=0; document.TSFE_ADMIN_PANEL_Form.submit(); return false;') . '"');
