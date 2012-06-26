fd8195d1b5dde7fd13765f07a35bc5552880022d
lorenzulrich <lorenzulrich@743128fe-103e-dd11-99c4-001b210b3e58>
2012-06-26 13:20:59 +0000

    Raising TYPO3 dependency to 4.5
    git-svn-id: https://svn.typo3.org/TYPO3v4/CoreProjects/feedit/trunk@4369 743128fe-103e-dd11-99c4-001b210b3e58

--- feeditadvanced/ext_emconf.php
+++ feeditadvanced/ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '1.6.0-dev',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '4.3.0-0.0.0',
+			'typo3' => '4.5.0-0.0.0',
 			'php' => '5.2.0-0.0.0',
 		),
 		'conflicts' => array(
